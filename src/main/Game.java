18ae244f9be92c4c14b65f35f62ca818dd901a27
Vlad Dementev <paacan228@yandex.com>
2025-01-06 06:33:27 +0300

    MyPlatGame - 1 (add main class)

---
+++ src/main/Game.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Game {
+
+    private WindowForGame windowForGame;
+    private GamePanel gamePanel;
+
+    public Game() {
+        gamePanel = new GamePanel();
+        windowForGame = new WindowForGame(gamePanel);
+
+    }
+}
---
+++ src/main/WindowForGame.java
@@ -0,0 +1,18 @@
+package main;
+
+import javax.swing.*;
+
+public class WindowForGame {
+    private JFrame jFrame;
+
+
+    public WindowForGame(GamePanel gamePanel) {
+        JFrame jframe = new JFrame();
+        jframe.setSize(400,400);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.add(gamePanel);
+        jframe.setVisible(true);
+
+
+    }
+}
